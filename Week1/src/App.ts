e8701d509798b2285b34b575ffd83a4f68ccc371
taebong1012 <dksxogus1012@gmail.com>
2023-12-20 23:27:59 +0900

    feat: 라우팅 처리

---
+++ Week1/src/App.ts
@@ -0,0 +1,27 @@
+import Home from "./pages/Home";
+import Solve from "./pages/Solve";
+
+const App = ($app: HTMLElement) => {
+  /** 라우팅 처리 함수 */
+  const router = () => {
+    const { pathname } = window.location;
+
+    if (pathname === "/") {
+      Home($app);
+    } else if (pathname === "/solve") {
+      Solve($app);
+    } else if (pathname === "/result") {
+    }
+  };
+
+  /** URL이 변경되면 app의 내용을 지우고 route 함수 실행 */
+  window.addEventListener("popstate", () => {
+    $app.innerHTML = "";
+    router();
+  });
+
+  /** routs 함수 실행 */
+  router();
+};
+
+export default App;
