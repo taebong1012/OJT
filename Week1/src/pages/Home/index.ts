ed4058da7c17171a06a4a513d63317e24ca6694a
taebong1012 <dksxogus1012@gmail.com>
2023-12-20 23:52:36 +0900

    feat: 홈 페이지 UI 구성 및 학습하기 버튼 구현

--- Week1/src/pages/Home/index.ts
+++ Week1/src/pages/Home/index.ts
@@ -1,9 +1,52 @@
-import Titlebar from "../../components/Titlebar";
+import jeilogo from "../../assets/svg/jeilogo.svg";
 
 const Home = ($app: HTMLElement) => {
-  const titlebar = Titlebar();
+  /** 홈 페이지 전체 레이아웃 */
+  const home: HTMLElement = document.createElement("div");
+  home.id = "home";
 
-  $app.appendChild(titlebar);
+  const container: HTMLElement = document.createElement("div");
+  container.id = "container";
+
+  const wrapper: HTMLElement = document.createElement("div");
+  wrapper.id = "wrapper";
+
+  const logo: HTMLImageElement = document.createElement("img");
+  logo.src = jeilogo;
+  logo.alt = "jei logo";
+  logo.width = 100;
+
+  const hometitle = document.createElement("span");
+  hometitle.id = "hometitle";
+  hometitle.textContent = "덧셈";
+
+  const homelevel = document.createElement("span");
+  homelevel.id = "homelevel";
+  homelevel.textContent = "E 1b";
+
+  /** 학습하기 버튼 */
+  const startbtn = document.createElement("button");
+  startbtn.id = "startbtn";
+  startbtn.textContent = "학습하기";
+
+  /** 학습하기 버튼 클릭 시 solve 페이지로 이동 */
+  startbtn.addEventListener("click", () => {
+    window.location.href = "/solve";
+  });
+
+  /** wrapper에 자식 노드들 추가 */
+  wrapper.appendChild(logo);
+  wrapper.appendChild(hometitle);
+  wrapper.appendChild(homelevel);
+
+  /** container에 자식 노드들 추가 */
+  container.appendChild(wrapper);
+  container.appendChild(startbtn);
+
+  /** home에 자식 노드 추가 */
+  home.appendChild(container);
+
+  $app.appendChild(home);
 };
 
 export default Home;
