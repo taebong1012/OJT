9ea70303e81b1eeb134a49acd2e5673448491975
taebong1012 <dksxogus1012@gmail.com>
2023-12-21 22:46:55 +0900

    feat: 페이지별 타이틀바 제목 변경

--- Week1/src/components/Common/Titlebar/index.ts
+++ Week1/src/components/Common/Titlebar/index.ts
@@ -3,25 +3,28 @@ const Titlebar = (): HTMLElement => {
   const titlebar: HTMLElement = document.createElement("div");
   titlebar.id = "titlebar";
 
+  const title: HTMLElement = document.createElement("div");
+  title.id = "title";
+
+  const maintitle: HTMLElement = document.createElement("span");
+
   /** 경로에 따라서 요소 변경 */
   const { pathname } = window.location;
   if (pathname === "/solve") {
-    const title: HTMLElement = document.createElement("div");
-    title.id = "title";
-
     /** 메인 타이틀 제목 */
-    const maintitle: HTMLElement = document.createElement("span");
     maintitle.textContent = "문제 해결";
-
-    /** 현재 학습 단계 레벨 */
-    const level: HTMLElement = document.createElement("span");
-    level.textContent = "E 1b";
-
-    title.appendChild(maintitle);
-    title.appendChild(level);
-
-    titlebar.appendChild(title);
+  } else if (pathname === "/result") {
+    maintitle.textContent = "학습 결과";
   }
+
+  /** 현재 학습 단계 레벨 */
+  const level: HTMLElement = document.createElement("span");
+  level.textContent = "E 1b";
+
+  title.appendChild(maintitle);
+  title.appendChild(level);
+
+  titlebar.appendChild(title);
 
   return titlebar;
 };
