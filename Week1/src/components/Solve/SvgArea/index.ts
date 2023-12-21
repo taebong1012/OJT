7b95bcf88ab7b26b1641e1746f9b82eda755452f
taebong1012 <dksxogus1012@gmail.com>
2023-12-21 14:12:54 +0900

    feat: svg의 뷰박스의 x좌표 이동을 통해서 svg 가운데 정렬

--- Week1/src/components/Solve/SvgArea/index.ts
+++ Week1/src/components/Solve/SvgArea/index.ts
@@ -16,7 +16,9 @@ const SvgArea = (num: number) => {
   /** svg의 높이, 너비, 뷰박스 설정 */
   svgArea.setAttribute("width", "880");
   svgArea.setAttribute("height", "200");
-  svgArea.setAttribute("viewBox", "0 -200 800 500");
+
+  /** 뷰박스의 x좌표 이동을 통해서 svg 가운데 정렬 */
+  svgArea.setAttribute("viewBox", `${(pathEndX + 108) / 2 - 400} -200 800 500`);
 
   /** def */
   const def = Def(num);
