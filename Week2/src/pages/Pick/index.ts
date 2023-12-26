320d34ec7d2a029ccf92e319639e85ecde717791
taebong1012 <dksxogus1012@gmail.com>
2023-12-26 16:44:41 +0900

    feat: 그림 고르기 페이지에 캔버스 구현

--- Week2/src/pages/Pick/index.ts
+++ Week2/src/pages/Pick/index.ts
@@ -1,6 +1,8 @@
 import Container from "@/components/common/Container";
 import Header from "@/components/common/Header";
 import Statement from "@/components/common/Statement";
+import animalsObjArr from "@/utils/Pick/animalsObjArr";
+import renderPickCanvas from "@/utils/Pick/renderPickCanvas";
 
 const Pick = ($app: HTMLElement) => {
   const header = Header();
@@ -11,10 +13,19 @@ const Pick = ($app: HTMLElement) => {
 
   const statement = Statement(1, "탈 것");
 
+  const pickCanvas = document.createElement("canvas");
+  pickCanvas.setAttribute("id", "pick-canvas");
+
+  /** 테스트 영역 확인 */
+  pickCanvas.style.border = "1px solid blue";
+
   container.appendChild(statement);
+  container.appendChild(pickCanvas);
 
   $app.appendChild(header);
   $app.appendChild(container);
+
+  renderPickCanvas();
 };
 
 export default Pick;
