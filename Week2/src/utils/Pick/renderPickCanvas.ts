115b7eef85221394de7a820bb6f488d0f2eaa9e2
taebong1012 <dksxogus1012@gmail.com>
2023-12-26 21:21:52 +0900

    feat: 랜덤 보기 svg 출력 및 클릭마우스 이벤트, 정답 여부 판단

--- Week2/src/utils/Pick/renderPickCanvas.ts
+++ Week2/src/utils/Pick/renderPickCanvas.ts
@@ -1,29 +1,58 @@
 import { fabric } from "fabric";
 
-const renderPickCanvas = () => {
+type obj = {
+  svgPath: string;
+  type: string;
+};
+
+const renderPickCanvas = (randomArr: obj[], handleOnClick: Function) => {
   const canvas = new fabric.Canvas("pick-canvas");
   canvas.setWidth(780);
   canvas.setHeight(400);
   /** 그룹 선택 비활성화 */
   canvas.selection = false;
 
-  for (let i = 0; i < 8; i++) {
-    const rect = new fabric.Rect({
-      left: (i % 4) * 200 + 20,
-      top: Math.floor(i / 4) * 180 + 40,
-      width: 140,
-      height: 140,
-      fill: "red",
-      hoverCursor: "pointer",
+  for (let i = 0; i < randomArr.length; i++) {
+    const { svgPath, type } = randomArr[i];
+
+    /** 보기 svg들 */
+    fabric.Image.fromURL(svgPath, (svg) => {
+      /** 보기 svg 속성 설정 */
+      svg.scaleToWidth(140);
+      svg.scaleToHeight(140);
+      svg.set({
+        left: (i % 4) * 200 + 20,
+        top: Math.floor(i / 4) * 180 + 40,
+        hoverCursor: "pointer",
+
+        /** 오브젝트 선택 비활성화 */
+        selectable: false,
+      });
+
+      /** 보기 그림 클릭했을 때 작동 */
+      svg.on("mousedown", () => {
+        const isCorrect = handleOnClick(type);
+        console.log(isCorrect);
+
+        /** 호버하면 기본 커서로 세팅 */
+        svg.set({
+          hoverCursor: "default",
+        });
+
+        /** 클릭 이벤트 제거 */
+        svg.off("mousedown");
+
+        /** 클릭되면 흑백처리 */
+        svg.filters!.push(new fabric.Image.filters.Grayscale());
+        svg.applyFilters();
+        canvas.renderAll();
+      });
+
+      canvas.add(svg);
     });
-
-    /** 오브젝트 선택 비활성화 */
-    rect.set("selectable", false);
-
-    canvas.add(rect);
   }
 
-  //   canvas.renderAll();
+  canvas.renderAll();
 };
 
 export default renderPickCanvas;
