bee68446b12c0cee9770aa9e353b203fc3015c29
taebong1012 <dksxogus1012@gmail.com>
2023-12-26 16:15:57 +0900

    fix: 그림 고르기 캔버스 레이아웃 4*2로 변경

--- Week2/src/utils/Pick/renderPickCanvas.ts
+++ Week2/src/utils/Pick/renderPickCanvas.ts
@@ -7,10 +7,10 @@ const renderPickCanvas = () => {
   /** 그룹 선택 비활성화 */
   canvas.selection = false;
 
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 8; i++) {
     const rect = new fabric.Rect({
-      left: (i % 3) * 220 + 96,
-      top: Math.floor(i / 3) * 180 + 40,
+      left: (i % 4) * 200 + 20,
+      top: Math.floor(i / 4) * 180 + 40,
       width: 140,
       height: 140,
       fill: "red",
