3518d0c08b1736fd341254f6501b5bf646f183f4
taebong1012 <dksxogus1012@gmail.com>
2024-01-03 08:37:10 +0900

    feat: atom 사용을 위한 디렉토리 생성 및 Provider 설정

--- Week3-4/src/App.tsx
+++ Week3-4/src/App.tsx
@@ -2,14 +2,17 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyle from "styles/GlobalStyle";
 import { lightTheme } from "styles/Themes";
 import Main from "pages/Main";
+import { Provider } from "jotai";
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={lightTheme}>
-        <GlobalStyle />
-        <Main />
-      </ThemeProvider>
+      <Provider>
+        <ThemeProvider theme={lightTheme}>
+          <GlobalStyle />
+          <Main />
+        </ThemeProvider>
+      </Provider>
     </>
   );
 }
