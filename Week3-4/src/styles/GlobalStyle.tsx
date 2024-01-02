15bebc1e8070680e46f2b5eba680b6d0de9281e4
taebong1012 <dksxogus1012@gmail.com>
2024-01-03 08:58:22 +0900

    feat: 폰트 설정

--- Week3-4/src/styles/GlobalStyle.tsx
+++ Week3-4/src/styles/GlobalStyle.tsx
@@ -5,13 +5,42 @@ const GlobalStyle = createGlobalStyle`
 
   ${reset}
 
+  @font-face {
+     font-family: 'S-CoreDream-6Bold';
+     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-6Bold.woff') format('woff');
+     font-weight: normal;
+     font-style: normal;
+}
+
+@font-face {
+     font-family: 'S-CoreDream-5Medium';
+     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-5Medium.woff') format('woff');
+     font-weight: normal;
+     font-style: normal;
+}
+
+@font-face {
+     font-family: 'S-CoreDream-4Regular';
+     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-4Regular.woff') format('woff');
+     font-weight: normal;
+     font-style: normal;
+}
+
+@font-face {
+     font-family: 'S-CoreDream-3Light';
+     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff') format('woff');
+     font-weight: normal;
+     font-style: normal;
+}
+
   *, *::before, *::after {
     box-sizing: border-box;
   }
 
   body {
-    font-family: "Helvetica", "Arial", sans-serif;
+    font-family: 'S-CoreDream-4Regular', "Helvetica", "Arial", sans-serif;
     line-height: 1.5;
+    padding: 0 30px;
     background-color: ${({ theme }) => theme.colors.background};
   }
 
