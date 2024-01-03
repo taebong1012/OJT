4fc8c09d956a7b7d2f48aedd021142c32ed989e0
taebong1012 <dksxogus1012@gmail.com>
2024-01-03 13:05:14 +0900

    feat: 기본 툴바 요소들 추가

--- Week3-4/src/components/ToolBar/Default/index.tsx
+++ Week3-4/src/components/ToolBar/Default/index.tsx
@@ -1,10 +1,17 @@
-import { RxPlusCircled } from "react-icons/rx";
+import {
+  RxPlusCircled,
+  RxText,
+  RxImage,
+  RxSquare,
+  RxBorderSolid,
+} from "react-icons/rx";
+import polygonSVG from "assets/svg/ic_polygon.svg";
 import Divider from "components/ToolBar/Divider";
 import NamedButton from "components/common/NamedButton";
 
 const Default = () => {
   const test = () => {
-    console.log("플러스 눌림");
+    console.log("테스트용 함수");
   };
 
   return (
@@ -12,7 +19,25 @@ const Default = () => {
       <NamedButton
         handleOnClick={test}
         icon={<RxPlusCircled />}
-        text="답안 보기 생성"
+        text="답안 보기 추가"
+      />
+      <Divider />
+      <NamedButton handleOnClick={test} icon={<RxText />} text="텍스트 추가" />
+      <NamedButton handleOnClick={test} icon={<RxImage />} text="이미지 추가" />
+      <NamedButton
+        handleOnClick={test}
+        icon={<RxSquare />}
+        text="사각형 추가"
+      />
+      <NamedButton
+        handleOnClick={test}
+        icon={<RxBorderSolid />}
+        text="직선 추가"
+      />
+      <NamedButton
+        handleOnClick={test}
+        icon={<img src={polygonSVG} />}
+        text="다각형 추가"
       />
       <Divider />
     </>
