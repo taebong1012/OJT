54e55034e12814b774ebaff1077efe6d35a8216d
taebong1012 <dksxogus1012@gmail.com>
2024-01-09 20:50:06 +0900

    refactor: MultipleTools 함수 분리

--- Week3-4/src/components/ToolBar/MultipleTools/index.tsx
+++ Week3-4/src/components/ToolBar/MultipleTools/index.tsx
@@ -3,18 +3,52 @@ import {
   RxAlignCenterHorizontally,
   RxAlignCenterVertically,
 } from "react-icons/rx";
-// import polygonSVG from "assets/svg/ic_polygon.svg";
 import Divider from "components/ToolBar/Divider";
 import NamedButton from "components/common/NamedButton";
-import {
-  makeCenterHorizontally,
-  makeCenterVertically,
-  makeGroup,
-} from "components/DrawingCanvas";
+import Controller from "controller/core";
+import fabric from "controller/fabric";
 
 const MultipleTools = () => {
-  const test = () => {
-    console.log("test");
+  const controller = Controller.getInstance();
+
+  /** 선택된 객체들 그룹화 */
+  const makeGroup = () => {
+    const activatedObject = controller.canvas!.getActiveObject();
+
+    if (activatedObject instanceof fabric.ActiveSelection) {
+      activatedObject.toGroup();
+      controller.canvas!.requestRenderAll();
+    } else {
+      console.error("makeGroup: no ActiveSelection");
+    }
+  };
+
+  /** 수평적으로 가운데 정렬 */
+  const makeCenterHorizontally = () => {
+    const activatedObject = controller.canvas!.getActiveObject();
+    if (activatedObject instanceof fabric.ActiveSelection) {
+      const activatedObjects = activatedObject.getObjects();
+      activatedObjects.forEach((obj) => {
+        obj.set("top", 0);
+      });
+      controller.canvas!.requestRenderAll();
+    } else {
+      console.error("makeCenterHorizontally: no ActiveSelection");
+    }
+  };
+
+  /** 수직적으로 가운데 정렬 */
+  const makeCenterVertically = () => {
+    const activatedObject = controller.canvas!.getActiveObject();
+    if (activatedObject instanceof fabric.ActiveSelection) {
+      const activatedObjects = activatedObject.getObjects();
+      activatedObjects.forEach((obj) => {
+        obj.set("left", 0);
+      });
+      controller.canvas!.requestRenderAll();
+    } else {
+      console.error("makeCenterHorizontally: no ActiveSelection");
+    }
   };
 
   return (
