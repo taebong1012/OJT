d8785e477039be1bffc7e4d152ec1aeeb8ea76a3
taebong1012 <dksxogus1012@gmail.com>
2024-01-10 16:58:08 +0900

    test: 그룹 시 정답 배열 업데이트 기능 구현 중

--- Week3-4/src/components/ToolBar/MultipleTools/index.tsx
+++ Week3-4/src/components/ToolBar/MultipleTools/index.tsx
@@ -7,19 +7,63 @@ import Divider from "components/ToolBar/Divider";
 import NamedButton from "components/common/NamedButton";
 import fabric from "controller/fabric";
 import Drawer from "Instance/Drawer";
+import { useAtom } from "jotai";
+import { answerObjectsAtom } from "atoms";
 
 const MultipleTools = () => {
   const drawer = Drawer.getInstance();
+  const [answerObjects, setAnswerObjects] = useAtom(answerObjectsAtom);
 
   /** 선택된 객체들 그룹화 */
   const makeGroup = () => {
     const activatedObject = drawer.canvas!.getActiveObject();
 
     if (activatedObject instanceof fabric.ActiveSelection) {
+
+      const updatedAnswerObjects = [...answerObjects];
+
+      activatedObject._objects.map((obj) => {
+        
+        const index = answerObjects.findIndex((answerObject) => {
+          let answerObjectId;
+          if (answerObject instanceof fabric.Group) {
+            answerObjectId = answerObject._objects[0].data.id;
+          } else {
+            answerObjectId = answerObject.data.id;
+          }
+          return answerObjectId === obj.data.id;
+        });
+
+        
+
+      });
+
       activatedObject.toGroup();
       drawer.canvas!.requestRenderAll();
     } else {
       console.error("makeGroup: no ActiveSelection");
+    }
+  };
+
+  /** answer 배열에서 id 찾아서 삭제 */
+  const deleteFromAnswerObjects = (targetObjId: string) => {
+    const index = answerObjects.findIndex((answerObject) => {
+      let answerObjectId;
+      if (answerObject instanceof fabric.Group) {
+        answerObjectId = answerObject._objects[0].data.id;
+      } else {
+        answerObjectId = answerObject.data.id;
+      }
+      return answerObjectId === targetObjId;
+    });
+
+    if (index !== -1) {
+      const updatedAnswerObjects = [...answerObjects];
+      updatedAnswerObjects.splice(index, 1);
+      setAnswerObjects(updatedAnswerObjects);
+    } else {
+      /** answerObject 배열에 없으므로 리턴 */
+      return;
     }
   };
 
