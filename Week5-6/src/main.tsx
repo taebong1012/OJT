2971a0958593fe8576651215ed9ff24a6c883fe4
taebong1012 <dksxogus1012@gmail.com>
2024-01-16 10:34:33 +0900

    chore: 프로젝트 절대경로 설정

--- Week5-6/src/main.tsx
+++ Week5-6/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./output.css";
+import "@/output.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
