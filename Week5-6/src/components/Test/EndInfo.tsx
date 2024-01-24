04449065299ccef40da40b8bd8575eaf77e5f3b2
taebong1012 <dksxogus1012@gmail.com>
2024-01-24 11:45:26 +0900

    feat: EndInfo 디자인 및 버튼 구현

--- Week5-6/src/components/Test/EndInfo.tsx
+++ Week5-6/src/components/Test/EndInfo.tsx
@@ -1,5 +1,32 @@
-const EndInfo = () => {
-  return <div></div>;
+import MainButton from "@/components/common/MainButton";
+import { useNavigate } from "react-router-dom";
+
+const EndInfo = ({ grade }: { grade: string | undefined }) => {
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    if (!grade) {
+      window.alert("ERR: no grade");
+      navigate("/");
+    } else {
+      navigate(`/result/${grade}`);
+    }
+  };
+
+  return (
+    <div className="w-[802px] h-[602px] mx-auto border-[1px] flex justify-center items-center bg-lightgrey rounded-10">
+      <div className="flex flex-col gap-[80px] items-center">
+        <div className="flex flex-col text-l font-bold">
+          <span className="font-extrabold text-2xl mb-[40px] text-primary">
+            수고하셨습니다!
+          </span>
+          <span className="mb-[16px]">평가가 종료되었습니다.</span>
+          <span>결과 확인 버튼을 눌러 결과를 확인해보세요!</span>
+        </div>
+        <MainButton text="결과 확인" handleOnClick={handleOnClick} />
+      </div>
+    </div>
+  );
 };
 
 export default EndInfo;
