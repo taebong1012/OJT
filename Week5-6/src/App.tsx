be94184654419da53c1182e45977f2c510bc769d
taebong1012 <dksxogus1012@gmail.com>
2024-01-25 13:48:12 +0900

    feat: 없는 경로일 경우 에러 페이지 노출

--- Week5-6/src/App.tsx
+++ Week5-6/src/App.tsx
@@ -4,6 +4,7 @@ import Result from "@/pages/Result";
 import Main from "@/pages/Main";
 import Test from "@/pages/Test";
 import PrivateRouter from "@/components/common/PrivateRouter";
+import ErrorPage from "@/pages/ErrorPage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route path="/start" element={<PrivateRouter authentication={false} />}>
         <Route index element={<Start />} />
       </Route>
+      <Route path="/*" element={<ErrorPage />} />
     </Routes>
   );
 }
---
+++ Week5-6/src/pages/ErrorPage.tsx
@@ -0,0 +1,26 @@
+import MainButton from "@/components/common/MainButton";
+import { doLogout } from "@/utils/doLogout";
+import { useNavigate } from "react-router-dom";
+import nofound from "@/assets/svg/ic_noFound.svg";
+
+const ErrorPage = () => {
+  const navigate = useNavigate();
+  const handleOnClick = () => {
+    doLogout();
+    navigate(-1);
+  };
+
+  return (
+    <div className="h-full w-screen min-w-[1240px] flex flex-col items-center justify-center">
+      <div className="mb-[40px] flex flex-col items-center">
+        <img src={nofound} className="w-[140px] mb-[20px]" />
+        <span className="text-l font-extrabold">
+          요청하신 페이지를 찾을 수 없습니다.
+        </span>
+      </div>
+      <MainButton text="뒤로 돌아가기" handleOnClick={handleOnClick} />
+    </div>
+  );
+};
+
+export default ErrorPage;
