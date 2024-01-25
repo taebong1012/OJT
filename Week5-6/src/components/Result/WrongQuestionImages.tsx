146a836da489efbded241db76b0672a05a6f3f54
taebong1012 <dksxogus1012@gmail.com>
2024-01-25 10:50:03 +0900

    fix: 틀린 문제 없을 경우 오답 보기 처리

--- Week5-6/src/components/Result/WrongQuestionImages.tsx
+++ Week5-6/src/components/Result/WrongQuestionImages.tsx
@@ -1,11 +1,14 @@
-type WrongQuestionImageProps = {
+import { useEffect, useState } from "react";
+
+type wrongQuestionImageType = {
   imageUrl: string;
   questionNum: number;
 };
+
 const WrongQuestionImage = ({
   imageUrl,
   questionNum,
-}: WrongQuestionImageProps) => {
+}: wrongQuestionImageType) => {
   return (
     <div className="w-[48%] min-h-[200px] flex flex-col border-2 rounded-10 border-lightgrey bg-white">
       <div className="flex justify-end pr-[30px] mb-[10px]">
@@ -18,11 +21,28 @@ const WrongQuestionImage = ({
   );
 };
 
-const WrongQuestion = ({
+const WrongQuestionImages = ({
   wrongQuestionData,
 }: {
   wrongQuestionData: Array<string | null>;
 }) => {
+  const [filteredWrongQuestion, setFilteredWrongQuestion] = useState<
+    Array<wrongQuestionImageType>
+  >([]);
+
+  useEffect(() => {
+    const filteredWrongQuestion = wrongQuestionData.filter((q) => q !== null);
+    const newWrongQuestion = filteredWrongQuestion.map(
+      (url, index) =>
+        ({
+          imageUrl: url,
+          questionNum: index,
+        } as wrongQuestionImageType)
+    );
+
+    setFilteredWrongQuestion(newWrongQuestion);
+  }, [wrongQuestionData]);
+
   return (
     <div className="w-full rounded-10 py-8 px-10 flex flex-col items-start shadow-default mb-[60px]">
       <span className="font-bold text-xl mb-1">오답 보기</span>
@@ -33,20 +53,26 @@ const WrongQuestion = ({
         <span className="font-bold">정답</span>입니다.
       </span>
       <div className="flex flex-wrap w-full gap-[34px]">
-        {wrongQuestionData.map((data, index) => {
-          if (data) {
-            return (
-              <WrongQuestionImage
-                key={index}
-                imageUrl={data}
-                questionNum={index + 1}
-              />
-            );
-          }
-        })}
+        {filteredWrongQuestion.length === 0 ? (
+          <div className="text-xl font-extrabold text-primary">
+            틀린 문제가 없습니다.
+          </div>
+        ) : (
+          wrongQuestionData.map((data, index) => {
+            if (data) {
+              return (
+                <WrongQuestionImage
+                  key={index}
+                  imageUrl={data}
+                  questionNum={index + 1}
+                />
+              );
+            }
+          })
+        )}
       </div>
     </div>
   );
 };
 
-export default WrongQuestion;
+export default WrongQuestionImages;
