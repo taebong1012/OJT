d00aff8e3e2634562621e90dd8f315637f8ba884
taebong1012 <dksxogus1012@gmail.com>
2024-02-01 15:45:09 +0900

    feat: 베어콩 캐릭터 생성 및 조작, 애니메이션 설정

--- Week7-8/src/scenes/volleyball/VolleyballStartScene.ts
+++ Week7-8/src/scenes/volleyball/VolleyballStartScene.ts
@@ -2,11 +2,14 @@ import backgroundImage from "@/assets/volleyball/background.svg";
 import groundImage from "@/assets/volleyball/ground.svg";
 import bejiPlayImage from "@/assets/volleyball/anim/beji-play.png";
 import bejiPlayJson from "@/assets/volleyball/anim/beji-play.json";
+import bearkongPlayImage from "@/assets/volleyball/anim/bearkong-play.png";
+import bearkongPlayJson from "@/assets/volleyball/anim/bearkong-play.json";
 import netImage from "@/assets/volleyball/net.svg";
 import whaleImage from "@/assets/volleyball/whale.png";
 
 export default class VolleyballStartScene extends Phaser.Scene {
   private bejiPlayer!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  private bearkongPlayer!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
 
   constructor() {
@@ -16,6 +19,7 @@ export default class VolleyballStartScene extends Phaser.Scene {
   preload() {
     this.load.image("background", backgroundImage);
     this.load.atlas("bejiPlay", bejiPlayImage, bejiPlayJson);
+    this.load.atlas("bearkongPlay", bearkongPlayImage, bearkongPlayJson);
     this.load.image("ground", groundImage);
     this.load.image("net", netImage);
     this.load.spritesheet("whale", whaleImage, {
@@ -53,6 +57,7 @@ export default class VolleyballStartScene extends Phaser.Scene {
 
     whale.play("whaleMove");
 
+    /** 베지 캐릭터 설정 */
     this.bejiPlayer = this.physics.add.sprite(
       200,
       443,
@@ -99,9 +104,58 @@ export default class VolleyballStartScene extends Phaser.Scene {
     this.bejiPlayer.play("bejiStay");
     this.bejiPlayer.setCollideWorldBounds(true);
     this.physics.add.collider(this.bejiPlayer, platforms);
+
+    /** 베어콩 캐릭터 설정 */
+    this.bearkongPlayer = this.physics.add.sprite(
+      200,
+      443,
+      "bearkongPlay",
+      "bearkong-play-1.png"
+    );
+
+    this.bearkongPlayer.anims.create({
+      key: "bearkongWalk",
+      frames: this.anims.generateFrameNames("bearkongPlay", {
+        prefix: "bearkong-",
+        start: 1,
+        end: 3,
+        suffix: ".png",
+      }),
+      repeat: -1,
+      yoyo: true,
+      frameRate: 6,
+    });
+
+    this.bearkongPlayer.anims.create({
+      key: "bearkongStay",
+      frames: this.anims.generateFrameNames("bearkongPlay", {
+        prefix: "bearkong-",
+        start: 4,
+        end: 5,
+        suffix: ".png",
+      }),
+      repeat: -1,
+      frameRate: 4,
+    });
+
+    this.bearkongPlayer.anims.create({
+      key: "bearkongJump",
+      frames: this.anims.generateFrameNames("bearkongPlay", {
+        prefix: "bearkong-",
+        start: 6,
+        end: 8,
+        suffix: ".png",
+      }),
+      frameRate: 8,
+    });
+
+    this.bearkongPlayer.play("bearkongStay");
+    this.bearkongPlayer.setCollideWorldBounds(true);
+    this.physics.add.collider(this.bearkongPlayer, platforms);
   }
 
   update(): void {
+    /** 베지 키보드 입력 설정 */
     const wKey = this.input.keyboard?.addKey("W");
     const aKey = this.input.keyboard?.addKey("A");
     const dKey = this.input.keyboard?.addKey("D");
@@ -128,6 +182,27 @@ export default class VolleyballStartScene extends Phaser.Scene {
       this.bejiPlayer.setVelocityX(0);
       if (this.bejiPlayer.body.blocked.down) {
         this.bejiPlayer.anims.play("bejiStay", true);
+      }
+    }
+
+    /** 베어콩 키보드 입력 설정 */
+    if (this.cursors.up.isDown && this.bearkongPlayer.body.blocked.down) {
+      this.bearkongPlayer.setVelocityY(-500);
+      this.bearkongPlayer.anims.play("bearkongJump");
+    } else if (this.cursors.left.isDown) {
+      this.bearkongPlayer.setVelocityX(-160);
+      if (this.bearkongPlayer.body.blocked.down) {
+        this.bearkongPlayer.anims.play("bearkongWalk", true);
+      }
+    } else if (this.cursors.right.isDown) {
+      this.bearkongPlayer.setVelocityX(160);
+      if (this.bearkongPlayer.body.blocked.down) {
+        this.bearkongPlayer.anims.play("bearkongWalk", true);
+      }
+    } else {
+      this.bearkongPlayer.setVelocityX(0);
+      if (this.bearkongPlayer.body.blocked.down) {
+        this.bearkongPlayer.anims.play("bearkongStay", true);
       }
     }
   }
