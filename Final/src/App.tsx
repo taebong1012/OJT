de17aab0affaa175f2911171b5e106ee765705e2
taebong1012 <dksxogus1012@gmail.com>
2024-02-13 15:21:33 +0900

    feat: 페이지 분리

--- Final/src/App.tsx
+++ Final/src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Main from "@/Pages/Main";
+import Specific from "@/Pages/Specific";
+import ErrorPage from "@/Pages/ErrorPage";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/week/:week" element={<Specific />} />
+      <Route path="/*" element={<ErrorPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ Final/src/Pages/Specific.tsx
@@ -0,0 +1,5 @@
+const Specific = () => {
+  return <div>스페시픽</div>;
+};
+
+export default Specific;
