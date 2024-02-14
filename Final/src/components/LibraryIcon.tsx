ed57fb24499f70c3172624daa4f639feb8d74e1e
taebong1012 <dksxogus1012@gmail.com>
2024-02-14 09:53:45 +0900

    feat: 라이브러리 이미지 호버 시 라이브러리 이름 노출

--- Final/src/components/LibraryIcon.tsx
+++ Final/src/components/LibraryIcon.tsx
@@ -1,11 +1,25 @@
 import getLibraryImg from "@/utils/getLibraryImg";
+import { useState } from "react";
 
 const LibraryIcon = ({ libraryName }: { libraryName: string }) => {
+  const [isHover, setIsHover] = useState(false);
+
   const libraryImgSrc: string | null = getLibraryImg(libraryName);
 
   return (
-    <div className="h-[30px] w-[30px] flex justify-center items-center">
+    <div
+      className="h-[30px] w-[30px] flex justify-center items-center relative"
+      onMouseOver={() => {
+        setIsHover(true);
+      }}
+      onMouseLeave={() => setIsHover(false)}
+    >
       {libraryImgSrc && <img src={libraryImgSrc} />}
+      {isHover && (
+        <div className="absolute top-[30px] text-[12px] bg-[#666666] p-[6px] rounded-[6px] z-10 border-[0.5px] border-[#FFFFFF]">
+          {libraryName}
+        </div>
+      )}
     </div>
   );
 };
