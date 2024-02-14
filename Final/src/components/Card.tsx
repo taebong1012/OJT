220853d497ac2a7f9322faed67d766ba1912d79d
taebong1012 <dksxogus1012@gmail.com>
2024-02-14 11:50:20 +0900

    feat: 새창에서 프로젝트 열기

--- Final/src/components/Card.tsx
+++ Final/src/components/Card.tsx
@@ -1,6 +1,5 @@
 import LibraryIcon from "@/components/LibraryIcon";
 import projectObjectType from "@/types/projectObjType";
-import { useNavigate } from "react-router-dom";
 
 const Card = ({
   projectNum,
@@ -9,12 +8,10 @@ const Card = ({
   projectTitle,
   usedLibrary,
 }: projectObjectType) => {
-  const navigate = useNavigate();
-
   return (
     <div
       className="border-[1px] border-[#666666] w-[380px] h-[260px] rounded-[10px] p-[20px] hover:scale-110 cursor-pointer transform transition duration-300"
-      onClick={() => navigate(`/week/${projectNum}`)}
+      onClick={() => window.open(`/week/${projectNum}`, "_blank")}
     >
       <div className="flex">
         <img
